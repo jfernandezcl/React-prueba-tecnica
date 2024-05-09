bb2aea204e1020b7b37cc070f26a601b9f95fcd8
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-10 00:40:56 +0200

    threeFirstWords

---
+++ prueba-1/README.md
@@ -0,0 +1,3 @@
+-Facts Random: https://catfact.ninja/fact
+
+- endpoint para usar: https://cataas.com/cat/says/${firtsWord}?fontSize=50&fontColor=red&json=true
--- prueba-1/src/App.jsx
+++ prueba-1/src/App.jsx
@@ -1,12 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = https://cataas.com/cat/says/${firtsWord}?fontSize=50&fontColor=red&json=true
+
+
 
 export function App () {
-	const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+	const [fact, setFact] = useState()
+	const [imageUrl, setImageUrl] = useState()
+
+	useEffect (() => {
+		fetch(CAT_ENDPOINT_RANDOM_FACT)
+		.then(res => res.json())
+		.then(data => {
+			const {fact} = data
+			setFact(data.fact)
+
+			const threeFirstWords = fact.split(' ').slice(0, 3).join(' ')
+			console.log(threeFirstWords)
+
+			
+			fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
+			.then(res => res.json)
+			.then(response => {
+				console.log(response)
+			})
+		})
+	}, [])
+
+
+
+
 
 	return (
 		<main>
 			<h1>App de gatos</h1>
-			<p>{fact}</p>
+			{fact && <p>{fact}</p>}
 		</main>
 	)
 }
