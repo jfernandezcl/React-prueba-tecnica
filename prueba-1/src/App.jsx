a4be8ce411300c8970ec562d715c1352ddc5ab0e
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-14 17:10:15 +0200

    API terminado

--- prueba-1/src/App.jsx
+++ prueba-1/src/App.jsx
@@ -15,14 +15,17 @@ export function App () {
 
 	return (
 		<main>
-			<div className="card">
-			<h1>Fetch Like a PRO</h1>
-			<ul>
+			
+			<h1 className="title">Fetch Like a PRO</h1>
+
+			<section>
+			<ul className="list">
 				{data?.map((user) => (
 					<li key={user.id}>{user.name}</li>
 				))}
 			</ul>
-			</div>
+			</section>
+		
 		</main>
 	);
 }
