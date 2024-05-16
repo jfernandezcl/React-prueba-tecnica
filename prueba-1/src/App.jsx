4c6b13fcf81264a27feedf961902ea6c88794d20
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-16 10:37:55 +0200

    Prueba

--- prueba-1/src/App.jsx
+++ prueba-1/src/App.jsx
@@ -1,33 +1,21 @@
-import { useFetch } from "../useFetch";
 import "./App.css";
+import { useFetch } from "../useFetch";
 
+export function App() {
+  const { data } = useFetch("https://jsonplaceholder.typicode.com/users");
 
-
-
-
-
-export function App(){
-	const { data } = useFetch("https://jsonplaceholder.typicode.com/users")
-
-	return (
-		<main>
-			
-			<h1 className="title ">Fetch Like a PRO</h1>
-
-			<section>
-			<ul className="list">
-				{data?.map((user) => (
-					<li key={user.id}>{user.name}</li>
-				))}
-			</ul>
-			</section>
-		
-		</main>
-	);
-
-
+  return (
+    <main>
+      <div className="containerTitle">
+        <h1 className="title">Fetch like a PRO</h1>
+      </div>
+      <section className="containerList">
+        <ul className="list">
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
 }
-
-
-
-
