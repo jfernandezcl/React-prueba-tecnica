1892502b060860e5f5767195922173bb50d572c8
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-18 01:32:36 +0200

    Estilos y title

--- prueba-1/src/App.jsx
+++ prueba-1/src/App.jsx
@@ -2,13 +2,14 @@ import { useDatosAPI } from "./hooks/useDatosAPI";
 import { Title } from "./components/Title/Title";
 import { ListOfUsers } from "./components/ListOfUsers/ListOfUsers";
 import "./App.css";
+import "./components/Title/Title.css";
 
 export function App() {
   const { data } = useDatosAPI("https://jsonplaceholder.typicode.com/users");
 
   return (
     <main>
-      <div>
+      <div className="title-data">
         <Title title={"Data API name"} />
       </div>
       <section>
